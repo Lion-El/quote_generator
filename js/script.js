b46140b81c936d7d6c0143240d6a69f47d09c285
Lion-El <68742917+Lion-El@users.noreply.github.com>
2020-09-15 14:39:20 +0100

    delete quote generator js

--- js/script.js
+++ js/script.js
@@ -10,7 +10,71 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
+const famousQuotes = [
+  {
+    quote: 'Spread love everywhere you go. Let no one ever come to you leaving happier.',
+    source: 'Mother Teressa'
+   },
+   {
+    quote: 'The way to get started is to quit talking and begin doing.',
+    source: 'Walt Disney'
+   },
+   {
+    quote: `Your time is limited, so don't waste it living someone else's life. Dont be 
+    trapped by dogma - which is living with the results of other people's thinking.`,
+    source: 'Steve Jobs'
+   },
+   {
+    quote: 'Spread love everywhere you go. Let no one ever come to you leaving happier.',
+    source: 'Mother Teressa'
+   },
+   {
+    quote: 'If life were predictable it would cease to be life, and be without flavour.',
+    source: 'Eleanor Roosevelt'
+   },
+   {
+    quote:`If you look at what you have in life, you'll always have more. if you look
+    what you don't have in life, you'll never have enough.`,
+    source: 'Oprah Winfrey'
+   },
+   {
+    quote: `if you sest your goals ridiculously high and it's a failure, you will fail 
+    above everyone else's success.`,
+    source: 'James Cameron'
+   },
+   {
+    quote: `Life is what happens when you're busy making other plans`,
+    source: 'John Lennon'
+   },
+   {
+    quote: `The best and most beautiful things in the world cannot be seen or even
+    touched - they must be felt with the heart.`,
+    source: 'Helen Keller'
+   },
+   {
+    quote: `It's during our darkest moments that we must focus to see the light.`,
+    source: 'Aristotle'
+   },
+   {
+    quote: `Do not go where the path may lead, go instead where there is no path and 
+    leave a trail.`,
+    source: 'Ralph Waldo Emerson'
+   },
+   {
+    quote: `The future belongs to those who believe in the beauty of their dreams.`,
+    source: 'Eleanor Roosevelt'
+   },
+   {
+    quote: `The greatest glory in living lies not in never falling, but in rising 
+    every time we fall.`,
+    source: 'Nelson Mandela'
+   },
+   {
+    quote: 'You wll face many defeats in life, but never let yourself be defeated.',
+    source: 'Maya Angelou'
+   }
 
+];
 
 
 /***
